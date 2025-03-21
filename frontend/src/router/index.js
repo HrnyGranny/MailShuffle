86b874610e2f6b92540c7c8ff5d0c34c378b9264
HornyGranny <91948162+HrnyGranny@users.noreply.github.com>
2025-03-21 12:57:11 +0000

    v0.0.10

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,8 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 import PresentationView from "../views/Presentation/PresentationView.vue";
 import AboutView from "../views/LandingPages/AboutUs/AboutView.vue";
 
-import ElBreadcrumbs from "../layouts/sections/elements/breadcrumbs/BreadcrumbsView.vue";
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,11 +14,6 @@ const router = createRouter({
       path: "/pages/landing-pages/about-us",
       name: "about",
       component: AboutView,
-    },
-    {
-      path: "/sections/elements/breadcrumbs",
-      name: "el-breadcrumbs",
-      component: ElBreadcrumbs,
     },
     {
       path: "/:pathMatch(.*)*",

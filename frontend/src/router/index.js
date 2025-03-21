c0321c1864181f282df79d73dc034b0c7f28c7a4
HornyGranny <91948162+HrnyGranny@users.noreply.github.com>
2025-03-21 12:48:58 +0000

    v0.0.9

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,12 +3,7 @@ import PresentationView from "../views/Presentation/PresentationView.vue";
 import AboutView from "../views/LandingPages/AboutUs/AboutView.vue";
 
 import ElBreadcrumbs from "../layouts/sections/elements/breadcrumbs/BreadcrumbsView.vue";
-import ElButtons from "../layouts/sections/elements/buttons/ButtonsView.vue";
-import ElButtonGroups from "../layouts/sections/elements/button-groups/ButtonGroupsView.vue";
-import ElDropdowns from "../layouts/sections/elements/dropdowns/DropdownsView.vue";
-import ElProgressBars from "../layouts/sections/elements/progress-bars/ProgressBarsView.vue";
-import ElToggles from "../layouts/sections/elements/toggles/TogglesView.vue";
-import ElTypography from "../layouts/sections/elements/typography/TypographyView.vue";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -26,36 +21,6 @@ const router = createRouter({
       path: "/sections/elements/breadcrumbs",
       name: "el-breadcrumbs",
       component: ElBreadcrumbs,
-    },
-    {
-      path: "/sections/elements/buttons",
-      name: "el-buttons",
-      component: ElButtons,
-    },
-    {
-      path: "/sections/elements/button-groups",
-      name: "el-button-groups",
-      component: ElButtonGroups,
-    },
-    {
-      path: "/sections/elements/dropdowns",
-      name: "el-dropdowns",
-      component: ElDropdowns,
-    },
-    {
-      path: "/sections/elements/progress-bars",
-      name: "el-progress-bars",
-      component: ElProgressBars,
-    },
-    {
-      path: "/sections/elements/toggles",
-      name: "el-toggles",
-      component: ElToggles,
-    },
-    {
-      path: "/sections/elements/typography",
-      name: "el-typography",
-      component: ElTypography,
     },
     {
       path: "/:pathMatch(.*)*",

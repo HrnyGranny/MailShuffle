1d86faa74b2d6e522f42c30d4bdeab919856b11c
Álvaro Ruiz Roldán <Alvaro.Ruiz12@alu.uclm.es>
2025-03-30 17:03:34 +0200

    v1.9.5

--- frontend/src/components/AboutUs/AboutUs.vue
+++ frontend/src/components/AboutUs/AboutUs.vue
@@ -1,5 +1,33 @@
 <script setup>
+import { onMounted } from "vue";
 import AboutUsOption from "../AboutUs/Components/AboutUsOption.vue";
+
+// Importar iconos desde assets
+import Button from "../../assets/img/buttons/SupportUs_button.png";
+
+// Función para cargar el botón de donación de PayPal
+const loadPayPalButton = () => {
+  const script = document.createElement("script");
+  script.src = "https://www.paypalobjects.com/donate/sdk/donate-sdk.js";
+  script.charset = "UTF-8";
+  script.onload = () => {
+    PayPal.Donation.Button({
+      env: "production",
+      hosted_button_id: "ZY2SHV8XNDW6G",
+      image: {
+        src: Button,
+        alt: "Donate with PayPal button",
+        title: "Support us with a donation",
+      },
+    }).render("#donate-button-container");
+  };
+  document.body.appendChild(script);
+};
+
+// Cargar el botón de PayPal al montar el componente
+onMounted(() => {
+  loadPayPalButton();
+});
 </script>
 
 <template>
@@ -13,19 +41,14 @@ import AboutUsOption from "../AboutUs/Components/AboutUsOption.vue";
             With our random email generator, you can create temporary inboxes to receive emails 
             without exposing your personal email address.
           </p>
-          <a
-            href="javascript:;"
-            class="text-custom icon-move-right"
-            :style="{ color: '#4E64EE' }"
-          >
-            Support Me
-            <i class="fas fa-arrow-right text-sm ms-1"></i>
-          </a>
+          <!-- Botón de soporte -->
+          <div id="donate-button-container" style="display: inline-block;"></div>
         </div>
+        <!-- Columna de opciones -->
         <div class="col-lg-6 mt-lg-0 mt-5 ps-lg-0 ps-0">
           <div class="pe-5">
             <AboutUsOption
-            :icon="{ component: 'schedule', backgroundColor: '#98FE98', iconColor: '#344767' }"
+              :icon="{ component: 'schedule', backgroundColor: '#98FE98', iconColor: '#344767' }"
               content="Emails last for 7 days, giving you enough time to manage your temporary inbox."
             />
             <AboutUsOption

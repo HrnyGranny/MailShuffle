7624f7d4c5affe040068d9b4a1a90ede4e3c12cb
Alvaro Ruiz Roldan <extaruizr@oesia.com>
2025-04-15 12:29:13 +0200

    v2.5.4

--- frontend/src/material-kit.js
+++ frontend/src/material-kit.js
@@ -1,6 +1,7 @@
 import "./assets/js/material-input.js";
 import "./assets/js/ripple-effect.js";
 import "./assets/scss/material-kit.scss";
+import "./assets/scss/argon-dashboard.scss";
 
 export default {
   install() {}
